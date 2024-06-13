2db01b092dbf9fe702ee1afa2cc64560df173ecc
Danil <47692381+SelDanilEv@users.noreply.github.com>
2024-06-13 20:11:11 +0400

    Rename

--- publish_common_nuget.bat
+++ publish_common_nuget.bat
@@ -1,5 +1,5 @@
 set /p API_KEY=<secrets/NugetApiKey
-dotnet nuget push Code/Defender.Common/src/Defender.Common/bin/Debug/^
+dotnet nuget push Apps/Defender.Common/src/Defender.Common/bin/Debug/^
 Defender.Common.1.2.22.nupkg ^
 --api-key %API_KEY%  ^
 --source https://api.nuget.org/v3/index.json
