f92f747479f30fdfd25be97d152c3de6bd253129
Danil <47692381+SelDanilEv@users.noreply.github.com>
2024-06-15 20:25:19 +0400

    Small changes

--- publish_message_broker_nuget.bat
+++ publish_message_broker_nuget.bat
@@ -1,6 +1,6 @@
 set /p API_KEY=<secrets/NugetApiKey
 dotnet nuget push Apps/Defender.Mongo.MessageBroker/src/Defender.Mongo.MessageBroker/bin/Debug/^
-Defender.Mongo.MessageBroker.8.1.1.nupkg ^
+Defender.Mongo.MessageBroker.8.1.2.nupkg ^
 --api-key %API_KEY% ^
 --source https://api.nuget.org/v3/index.json
 pause
