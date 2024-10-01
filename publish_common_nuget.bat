709ade6ba61dac75c621f05c1e28dc617cfd3554
Danil <47692381+SelDanilEv@users.noreply.github.com>
2024-10-01 16:13:22 +0200

    Adding new service + adding https

--- publish_common_nuget.bat
+++ publish_common_nuget.bat
@@ -1,6 +1,6 @@
 set /p API_KEY=<secrets/NugetApiKey
 dotnet nuget push Apps/Defender.Common/src/Defender.Common/bin/Debug/^
-Defender.Common.1.2.29.nupkg ^
+Defender.Common.1.2.30.nupkg ^
 --api-key %API_KEY%  ^
 --source https://api.nuget.org/v3/index.json
 pause
