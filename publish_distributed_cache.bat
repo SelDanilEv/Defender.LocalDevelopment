ef01d767b06c8cc70e5428e55613b82812e58b19
Danil <47692381+SelDanilEv@users.noreply.github.com>
2024-12-10 23:26:07 +0100

    Improvements

--- publish_distributed_cache.bat
+++ publish_distributed_cache.bat
@@ -1,6 +1,6 @@
 set /p API_KEY=<secrets/NugetApiKey
 dotnet nuget push Apps/Defender.DistributedCache/src/Defender.DistributedCache/bin/Debug/^
-Defender.DistributedCache.0.1.2.nupkg ^
+Defender.DistributedCache.0.1.3.nupkg ^
 --api-key %API_KEY% ^
 --source https://api.nuget.org/v3/index.json
 pause
