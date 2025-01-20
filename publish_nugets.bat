ac906959cb6900edb3f1e34f4d8dd678591c0abe
Danil <47692381+SelDanilEv@users.noreply.github.com>
2025-01-20 16:14:30 +0100

    Update common nuget version

--- publish_nugets.bat
+++ publish_nugets.bat
@@ -1,23 +1,27 @@
 set /p API_KEY=<secrets/NugetApiKey
 
 dotnet nuget push Apps/Defender.Common/src/Defender.Common/bin/Debug/^
-Defender.Common.1.6.1.nupkg ^
---api-key %API_KEY%  ^
---source https://api.nuget.org/v3/index.json
+Defender.Common.1.6.2.nupkg ^
+--api-key %API_KEY% ^
+--source https://api.nuget.org/v3/index.json ^
+--skip-duplicate
 
 dotnet nuget push Apps/Defender.Kafka/src/Defender.Kafka/bin/Debug/^
 Defender.Kafka.8.0.5.nupkg ^
---api-key %API_KEY%  ^
---source https://api.nuget.org/v3/index.json
+--api-key %API_KEY% ^
+--source https://api.nuget.org/v3/index.json ^
+--skip-duplicate
 
 dotnet nuget push Apps/Defender.DistributedCache/src/Defender.DistributedCache/bin/Debug/^
 Defender.DistributedCache.0.1.6.nupkg ^
 --api-key %API_KEY% ^
---source https://api.nuget.org/v3/index.json
+--source https://api.nuget.org/v3/index.json ^
+--skip-duplicate
 
 dotnet nuget push Apps/Defender.Mongo.MessageBroker/src/Defender.Mongo.MessageBroker/bin/Debug/^
 Defender.Mongo.MessageBroker.8.1.5.nupkg ^
 --api-key %API_KEY% ^
---source https://api.nuget.org/v3/index.json
+--source https://api.nuget.org/v3/index.json ^
+--skip-duplicate
 
 pause
