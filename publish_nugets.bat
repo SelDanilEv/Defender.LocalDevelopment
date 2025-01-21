2a0517380768e294a519d9a118fa8b2466ab3ffe
Danil <47692381+SelDanilEv@users.noreply.github.com>
2025-01-21 22:52:19 +0100

    v1.6.3

--- publish_nugets.bat
+++ publish_nugets.bat
@@ -1,7 +1,7 @@
 set /p API_KEY=<secrets/NugetApiKey
 
 dotnet nuget push Apps/Defender.Common/src/Defender.Common/bin/Debug/^
-Defender.Common.1.6.2.nupkg ^
+Defender.Common.1.6.3.nupkg ^
 --api-key %API_KEY% ^
 --source https://api.nuget.org/v3/index.json ^
 --skip-duplicate
